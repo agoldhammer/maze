ef9c503cc493a7b44ad5b90f213c97b48c73bcfb
Art Goldhammer <art.goldhammer@gmail.com>
2019-09-09 14:57:15 -0400

    changed tests to use redefs

--- test/maze/paral_test.clj
+++ test/maze/paral_test.clj
@@ -13,50 +13,54 @@
   #_(mpar/init-run))
 
 (deftest test-closed-functions
-  (testing "functions dealing with closed map"
-    (mpar/reset-all)
-    (let [closed (atom {})
-          node (tu/make-dummy-node)
-          ibuff (mbuff/hash-of-loc (:loc node) mp/nthreads)]
-      (mbuff/put-closed closed node)
-      (is (= node (mbuff/find-in-closed closed (:loc node))))
-      (mbuff/remove-from-closed closed node)
-      (is (nil? (mbuff/find-in-closed closed (:loc node))))
-      (mbuff/put-closed (mbuff/closed-locs ibuff) node)
-      (is (= node (mbuff/find-in-closed (:loc node)))))))
+  (with-redefs [mp/nthreads 4]
+    (testing "functions dealing with closed map"
+      (mpar/reset-all)
+      (let [closed (atom {})
+            node (tu/make-dummy-node)
+            ibuff (mbuff/hash-of-loc (:loc node) mp/nthreads)]
+        (mbuff/put-closed closed node)
+        (is (= node (mbuff/find-in-closed closed (:loc node))))
+        (mbuff/remove-from-closed closed node)
+        (is (nil? (mbuff/find-in-closed closed (:loc node))))
+        (mbuff/put-closed (mbuff/closed-locs ibuff) node)
+        (is (= node (mbuff/find-in-closed (:loc node))))))))
 
 (deftest test-process-successors
-  (testing "routing of successors for trivial maze"
-    (setup-trivial-test)
-    (mpar/process-successors (mb/start-node) (mbuff/open-qs 0))
-    (let [v (mapv mbuff/poll-buff mbuff/input-buffs)]
-      (is (= [2 0] (:loc (get-in v [0 1]))))
-      (is (= [1 1] (:loc (get-in v [1 1]))))
-      (is (= [0 0] (:loc (get-in v [2 1]))))
-      (is (nil? (v 3))))))
-
-(deftest test-intake
-  (testing "intake from input buffers"
-    (setup-trivial-test)
-    (let [ibuff (mbuff/hash-of-loc (:loc (mb/start-node)) mp/nthreads)
-          input (mbuff/input-buffs ibuff)
-          closed (mbuff/closed-locs ibuff)
-          open (mbuff/open-qs ibuff)] ; ibuff will equal 3
-      (mbuff/put-buff input [0 (mb/start-node)])
-      (mpar/intake-from-buff input closed open)
-      (is (= [0 (mb/start-node)] (mbuff/poll-buff open))))))
-
-(deftest test-expand-open
-  (testing "expand-open"
-    (setup-trivial-test)
-    (let [open (mbuff/open-qs 0)
-          closed (mbuff/closed-locs 0)]
-      (mbuff/put-buff open [0 (mb/start-node)])
-      (mpar/expand-open closed open)
+  (with-redefs [mp/nthreads 4]
+    (testing "routing of successors for trivial maze"
+      (setup-trivial-test)
+      (mpar/process-successors (mb/start-node) (mbuff/open-qs 0))
       (let [v (mapv mbuff/poll-buff mbuff/input-buffs)]
         (is (= [2 0] (:loc (get-in v [0 1]))))
         (is (= [1 1] (:loc (get-in v [1 1]))))
         (is (= [0 0] (:loc (get-in v [2 1]))))
-        (is (nil? (v 3))))
-      (let [closed-node (mbuff/find-in-closed closed (:loc (mb/start-node)))]
-        (is (= (mb/start-node) closed-node))))))
+        (is (nil? (v 3)))))))
+
+(deftest test-intake
+  (with-redefs [mp/nthreads 4]
+    (testing "intake from input buffers"
+      (setup-trivial-test)
+      (let [ibuff (mbuff/hash-of-loc (:loc (mb/start-node)) mp/nthreads)
+            input (mbuff/input-buffs ibuff)
+            closed (mbuff/closed-locs ibuff)
+            open (mbuff/open-qs ibuff)] ; ibuff will equal 3
+        (mbuff/put-buff input [0 (mb/start-node)])
+        (mpar/intake-from-buff input closed open)
+        (is (= [0 (mb/start-node)] (mbuff/poll-buff open)))))))
+
+(deftest test-expand-open
+  (with-redefs [mp/nthreads 4]
+    (testing "expand-open"
+      (setup-trivial-test)
+      (let [open (mbuff/open-qs 0)
+            closed (mbuff/closed-locs 0)]
+        (mbuff/put-buff open [0 (mb/start-node)])
+        (mpar/expand-open closed open)
+        (let [v (mapv mbuff/poll-buff mbuff/input-buffs)]
+          (is (= [2 0] (:loc (get-in v [0 1]))))
+          (is (= [1 1] (:loc (get-in v [1 1]))))
+          (is (= [0 0] (:loc (get-in v [2 1]))))
+          (is (nil? (v 3))))
+        (let [closed-node (mbuff/find-in-closed closed (:loc (mb/start-node)))]
+          (is (= (mb/start-node) closed-node)))))))
